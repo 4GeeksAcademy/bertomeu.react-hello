e847cc41e7d6a7d18699ee2639aa2a201afaf121
bertomeu <josecarlosbertomeu@gmail.com>
2024-07-08 16:15:26 +0000

    Add Footer.js

---
+++ src/js/component/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="footer py-4" style={{ backgroundColor: "#222529", marginTop: "8rem"}}>
+            <div className="container-fluid text-center">
+                <span className="text-white">Copyright © Your Website 2019</span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
